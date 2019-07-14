d7f4713c4450d5bff21dd7645fef736864258232
Marcin Budzinski <marcin.budzinski.x@gmail.com>
2019-07-14 14:09:11 +0200

    user back end

---
+++ src/main/java/com/example/demoshopmarcin/categories/Category.java
@@ -0,0 +1,15 @@
+package com.example.demoshopmarcin.categories;
+
+import com.example.demoshopmarcin.BaseEntity;
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class Category extends BaseEntity {
+}
