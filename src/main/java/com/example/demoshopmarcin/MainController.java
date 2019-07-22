64a1a29e3d27f76889b9f9b3f05725414b258385
Marcin Budzinski <marcin.budzinski.x@gmail.com>
2019-07-22 15:40:26 +0200

    registration and login first part

--- src/main/java/com/example/demoshopmarcin/MainController.java
+++ src/main/java/com/example/demoshopmarcin/MainController.java
@@ -1,10 +1,16 @@
 package com.example.demoshopmarcin;
 
+import com.example.demoshopmarcin.users.UserRegistrationDTO;
 import com.example.demoshopmarcin.users.UserRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class MainController {
@@ -16,14 +22,28 @@ public class MainController {
         this.userRegistrationService = userRegistrationService;
     }
 
-    @GetMapping("/")
-    public String home(){
+    @GetMapping(value = "/")
+    public String home(Model model){
+        model.addAttribute("userData", new UserRegistrationDTO());
         return"index";
+    }
+
+    @PostMapping(value = "/")
+    public String registerUser(@ModelAttribute(name = "userData") @Valid UserRegistrationDTO userRegistrationDTO, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){return "register";}
+        return "registerDone";
     }
 
     @GetMapping(value = "/register")
     public String registerForm(Model model){
-        return "registerForm";
+        model.addAttribute("userData", new UserRegistrationDTO());
+
+        return "register";
     }
 
-}
+   // @PostMapping(value = "/register")
+   // public String registerUser(@ModelAttribute(name = "userData") @Valid UserRegistrationDTO userRegistrationDTO, BindingResult bindingResult){
+   //     if (bindingResult.hasErrors()){return "register";}
+    //    return "registerDone";
+    }
+
